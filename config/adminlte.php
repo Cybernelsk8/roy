54c324978391558fdd8c954c9fe7c310fcbde334
Cybernelsk8 <nelson.o.vasquez@gmail.com>
2021-03-11 23:07:40 -0600

    Creacion de una policy

--- config/adminlte.php
+++ config/adminlte.php
@@ -46,7 +46,7 @@ return [
     */
 
     'logo' => '<b>P</b>lataforma',
-    'logo_img' => asset('img/other/favicon.png'),
+    'logo_img' => 'img/other/favicon.png',
     'logo_img_class' => 'brand-image',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
